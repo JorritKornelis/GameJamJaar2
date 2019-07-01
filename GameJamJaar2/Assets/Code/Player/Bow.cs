2725b413dd35eb24cfd6cf27956483a5dc94715f
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-07-01 16:29:08 +0200

    save

--- GameJamJaar2/Assets/Code/Player/Bow.cs
+++ GameJamJaar2/Assets/Code/Player/Bow.cs
@@ -8,7 +8,7 @@ public class Bow : MonoBehaviour
     public GameObject arrowPos;
     public bool gotArrow = true;
     public float flySpeed;
-    float  arrowDis;
+    public float flySpeedBack;
     GameObject player;
     public LayerMask collMask;
 
@@ -26,8 +26,11 @@ public class Bow : MonoBehaviour
         if (gotArrow == false && Physics.CheckSphere(player.transform.position, player.GetComponent<SphereCollider>().radius, collMask))
         {
             arrow.transform.position = arrowPos.transform.position;
+            arrow.transform.rotation = arrowPos.transform.rotation;
+
             arrow.GetComponent<Rigidbody>().isKinematic = true;
             arrow.transform.parent = player.transform;
+
             StartCoroutine(WaitBetweenShots());
         }
     }
@@ -40,25 +43,25 @@ public class Bow : MonoBehaviour
             arrow.transform.parent = null;
             StartCoroutine(WaitBetweenShots());
 
-            arrow.GetComponent<Rigidbody>().AddForce(transform.forward * flySpeed * 10f);
+            arrow.GetComponent<Rigidbody>().AddForce(-transform.forward * flySpeed * 10f);
         }
     }
 
     IEnumerator WaitBetweenShots()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.2f);
         gotArrow = !gotArrow;
     }
 
     void RetrieveArrow()
     {
-        if (Input.GetButtonDown("Fire1") && gotArrow == false)
+        if (Input.GetButton("Fire1") && gotArrow == false)
         {
             player.GetComponent<PlayerControler>().mayMoveBool = false;
 
             Vector3 tempDir = arrow.transform.position - player.transform.position;
             arrow.transform.LookAt(player.transform);
-            arrow.GetComponent<Rigidbody>().AddForce(-tempDir * flySpeed);
+            arrow.GetComponent<Rigidbody>().velocity = -tempDir * flySpeedBack;
 
         }
 
