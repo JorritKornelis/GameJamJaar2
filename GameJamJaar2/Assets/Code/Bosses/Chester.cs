3f634b44578f3dcb2f4e3a0ec05c5ebd5a8eb00b
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-07-03 16:28:28 +0200

    Puash

--- GameJamJaar2/Assets/Code/Bosses/Chester.cs
+++ GameJamJaar2/Assets/Code/Bosses/Chester.cs
@@ -2,17 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chester : MonoBehaviour
+public class Chester : BossBase
 {
-    // Start is called before the first frame update
-    void Start()
+    public string playertag;
+    public float moveSpeed;
+    public float jumpHeight;
+    public float jumpSpeed;
+    public float moveCooldown;
+
+    public int coinAmount;
+    public float coinFireDelay;
+
+    public void Start()
     {
-        
+        StartCoroutine(CheckAttack());
     }
 
-    // Update is called once per frame
-    void Update()
+    public override IEnumerator CheckAttack()
     {
-        
+        yield return new WaitForSeconds(moveCooldown);
+        StartCoroutine(JumpToPlayer());
+    }
+
+    public override void Damage()
+    {
+
+    }
+
+    public IEnumerator CoinShotAir()
+    {
+        for (int i = 0; i < coinAmount; i++)
+        {
+            yield return new WaitForSeconds(coinFireDelay);
+        }
+    }
+
+    public IEnumerator JumpToPlayer()
+    {
+        GameObject player = GameObject.FindWithTag(playertag);
+        transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+        float currentHeight = jumpSpeed;
+        float normalHeight = transform.position.y;
+        while(currentHeight > 0)
+        {
+            if (currentHeight > jumpSpeed / 2f)
+                transform.Translate(Vector3.up * jumpHeight * Time.deltaTime);
+            else
+                transform.Translate(-Vector3.up * jumpHeight * Time.deltaTime);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            currentHeight -= Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = new Vector3(transform.position.x, normalHeight, transform.position.z);
+        StartCoroutine(CheckAttack());
     }
 }
