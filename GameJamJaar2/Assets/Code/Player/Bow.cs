313304f27c01d55e18ac0371b8a06aaaa219b407
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-07-03 15:16:00 +0200

    nog een bow fix

--- GameJamJaar2/Assets/Code/Player/Bow.cs
+++ GameJamJaar2/Assets/Code/Player/Bow.cs
@@ -6,6 +6,7 @@ public class Bow : MonoBehaviour
 {
     public GameObject arrow;
     public GameObject arrowPos;
+    public bool mayShootArrow = true;
     public bool gotArrow = true;
     public bool zoekPijl = false;
     public float flySpeed;
@@ -27,15 +28,18 @@ public class Bow : MonoBehaviour
     {
         RetrieveArrow();
         FireBow();
+        
         //get arrow
-        if (gotArrow == false&& zoekPijl==true && Physics.CheckSphere(player.transform.position, player.GetComponent<SphereCollider>().radius, collMask))
+        if (mayShootArrow == false&& zoekPijl==true && Physics.CheckSphere(player.transform.position, player.GetComponent<SphereCollider>().radius, collMask))
         {
             arrow.GetComponent<Rigidbody>().isKinematic = true;
             arrow.transform.parent = player.transform;
 
             arrow.transform.position = arrowPos.transform.position;
             arrow.transform.rotation = arrowPos.transform.rotation;
+            arrow.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
 
+            gotArrow = true;
             zoekPijl = false;
             if (zoekPijl == false)
             {
@@ -46,9 +50,8 @@ public class Bow : MonoBehaviour
         if (b == true && arrow.transform.position.y <= 0.2f)
         {
             arrow.GetComponent<Rigidbody>().isKinematic = true;
-            //b = false;
         }
-        else if (b==false &&gotArrow == false)
+        else if (b==false && gotArrow == false)
         {
             arrow.GetComponent<Rigidbody>().isKinematic = false;
         }
@@ -56,7 +59,7 @@ public class Bow : MonoBehaviour
 
     void FireBow()
     {
-        if (Input.GetButton("Fire1") && gotArrow == true)
+        if (Input.GetButton("Fire1") && mayShootArrow == true)
         {
             player.GetComponent<PlayerControler>().mayMoveBool = false;
             if (chargeTimer <= 1)
@@ -64,13 +67,15 @@ public class Bow : MonoBehaviour
                 chargeTimer += Time.deltaTime;
             }
         }
-        else if (Input.GetButtonUp("Fire1") && gotArrow == true && chargeTimer >= 0.5f)
+        else if (Input.GetButtonUp("Fire1") && mayShootArrow == true && chargeTimer >= 0.5f)
         {
             arrow.GetComponent<Rigidbody>().isKinematic = false;
             arrow.transform.parent = null;
             StartCoroutine(WaitBetweenShots());
-            zoekPijl = true;
             chargeTimer = chargeTimerReset;
+
+            zoekPijl = true;
+            gotArrow = false;
 
             arrow.GetComponent<Rigidbody>().AddForce(-transform.forward * flySpeed * 10f);
             b = true;
@@ -81,13 +86,13 @@ public class Bow : MonoBehaviour
     IEnumerator WaitBetweenShots()
     {
         yield return new WaitForSeconds(0.3f);
-        gotArrow = !gotArrow;
-        Debug.Log(gotArrow + " Got Arrow");
+        mayShootArrow = !mayShootArrow;
+        Debug.Log(mayShootArrow + " Got Arrow");
     }
 
     void RetrieveArrow()
     {
-        if (Input.GetButton("Fire1") && gotArrow == false)
+        if (Input.GetButton("Fire1") && mayShootArrow == false)
         {
             b = false;
             player.GetComponent<PlayerControler>().mayMoveBool = false;
@@ -100,7 +105,7 @@ public class Bow : MonoBehaviour
         if (Input.GetButtonUp("Fire1"))
         {
             player.GetComponent<PlayerControler>().mayMoveBool = true;
-            if (!gotArrow)
+            if (!mayShootArrow)
             {
                 b = true;
             }
