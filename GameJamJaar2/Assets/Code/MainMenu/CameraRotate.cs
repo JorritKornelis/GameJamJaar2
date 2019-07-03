7b2c5b96149be8dccdc3a014bc09fc039beb802e
MikeyBuchan <psmbgames@hotmail.com>
2019-07-03 12:58:23 +0200

    New Scene

---
+++ GameJamJaar2/Assets/Code/MainMenu/CameraRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public GameObject focusPoint;
+    public float movSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Focus();
+    }
+
+    public void Focus()
+    {
+        transform.LookAt(focusPoint.transform);
+
+        transform.Translate(movSpeed * Time.deltaTime, 0, 0);
+    }
+}
