1fa4ebceb67084d9dc6a06a29f07287ff16045fe
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-07-03 12:56:06 +0200

    start portal script

---
+++ GameJamJaar2/Assets/Code/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public ParticleSystem portalPar;
+    public string sceneSwitchName;
+
+    void PlayThePartical()
+    {
+        portalPar.Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && portalPar.isEmitting)
+        {
+            SceneManager.LoadScene(sceneSwitchName);
+        }
+    }
+
+
+}
