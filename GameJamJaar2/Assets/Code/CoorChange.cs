a63ceee894df75e90b6bb43710a3013e74c55f62
MikeyBuchan <psmbgames@hotmail.com>
2019-07-04 16:03:04 +0200

    MainMenu

---
+++ GameJamJaar2/Assets/Code/CoorChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CoorChange : MonoBehaviour
+{
+    public Color color1, color2;
+    public MeshRenderer obj;
+    public Color mCol1, mCol2;
+    public Text text;
+
+    public void ChangeColor(int index)
+    {
+        text.color = ((index == 1) ? color1 : color2);
+        obj.material.color = (index == 1) ? mCol1 : mCol2;
+    }
+}
